bad40646e4456acfbdb0019ccf9e0bcb13e5a0c7
Maks <mn27305@gmail.com>
2020-06-08 14:37:45 +0200

    93 11.00

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,8 @@ import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
+import { Button } from 'reactstrap';
+/* import style from './App.module.css'; */
 import './app.css';
 
 
